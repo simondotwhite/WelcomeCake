85c543f94e6a4f633492932c13ea8227748b01b2
Simon White <mail@simonwhite.co.uk>
2012-01-30 22:35:08 +0000

    Inventory Checks
    Checks inventory for a free slot and that they don't already have cake.
    If both of these are true it gives them cake and send them a welcome
    message.

--- src/sekonda/bukkit/HaveCake/Main.java
+++ src/sekonda/bukkit/HaveCake/Main.java
@@ -2,41 +2,53 @@ package sekonda.bukkit.HaveCake;
 
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
  
 public class Main extends JavaPlugin implements Listener {
  
 	Logger log = Logger.getLogger("Minecraft");
+	PluginDescriptionFile pdf = this.getDescription();
  
 	public void onEnable(){
-		log.info("HaveCake has been enabled.");
+		//register events
 		getServer().getPluginManager().registerEvents(this, this);
+		log.info(pdf.getName() + "has been enabled.");
 	}
 	
 	@EventHandler
-    public void onLogin(PlayerLoginEvent event) {
+    public void onLogin(PlayerJoinEvent event) {
 		//Save Player and welcome them
         Player player = event.getPlayer();
-        player.sendMessage("Welcome " + player +" to the server, have some cake.");
-        
-        //Add the cake to Inventory
-        PlayerInventory inventory = player.getInventory(); 
+        String playerName = player.getName(); 
+    
+        //Check to see if they don't have cake first.
+        PlayerInventory inventory = player.getInventory();         
         ItemStack cake = new ItemStack(Material.CAKE,1); 
-        inventory.addItem(cake);
         
-        //Send to console
-        log.info("[HaveCake] " + player + " was given cake.");
+        //Gets next empty slot
+        int empty = inventory.firstEmpty();
+        
+        //Checks if there is an empty slot and they don't already have cake.
+        if(empty != -1 && !inventory.contains(Material.CAKE)) {
+           	inventory.addItem(cake);
+           	player.sendMessage("Welcome! " + ChatColor.GOLD + playerName + ChatColor.WHITE  + ". You had a space in your inventory, so we gave you some cake.");
+           	
+           	//Log to console
+           	log.info(pdf.getName() + " added cake to " + playerName);
+        }       
     }
 	
 	public void onDisable(){
-		log.info("HaveCake has been disabled.");
+		log.info(pdf.getName() + " has been disabled.");
 	}
 }
 
