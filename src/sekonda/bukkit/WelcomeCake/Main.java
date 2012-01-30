08e887e9aa325479e0d4397cf35ac77995d1f1a4
Simon White <mail@simonwhite.co.uk>
2012-01-30 23:14:34 +0000

    Removed Welcome Message
    Removed the Welcome {Player} to our server bit as it's not needed. Cake
    message is still there.
    
    Builder / plugin.yml seemed to require editing, again.

---
+++ src/sekonda/bukkit/WelcomeCake/Main.java
@@ -0,0 +1,63 @@
+package sekonda.bukkit.WelcomeCake;
+
+import java.util.logging.Logger;
+import org.bukkit.Material;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+ 
+public class Main extends JavaPlugin implements Listener {
+ 
+	Logger log = Logger.getLogger("Minecraft");
+ 
+	public void onEnable(){
+		//Sort Prefix
+		PluginDescriptionFile pdf = this.getDescription();
+		final String pluginPrefix = "[" + pdf.getName() + "] ";
+		//register events
+		getServer().getPluginManager().registerEvents(this, this);
+		//Send to console
+		log.info(pluginPrefix + "has been enabled.");
+	}
+	
+	@EventHandler
+    public void onLogin(PlayerJoinEvent event) {
+		//Sort Prefix
+		PluginDescriptionFile pdf = this.getDescription();
+		final String pluginPrefix = "[" + pdf.getName() + "] ";
+		//Save Player and welcome them
+        Player player = event.getPlayer();
+        String playerName = player.getName(); 
+        
+        //Check to see if they don't have cake first.
+        PlayerInventory inventory = player.getInventory();         
+        ItemStack cake = new ItemStack(Material.CAKE,1); 
+        
+        //Gets next empty slot
+        int empty = inventory.firstEmpty();
+        
+        //Checks if there is an empty slot and they don't already have cake.
+        if(empty != -1 && !inventory.contains(Material.CAKE)) {
+           	//Adds cake
+        	inventory.addItem(cake);
+           	//Sends welcome message
+        	player.sendMessage("You had a space in your inventory, so we gave you some cake.");
+           	//Log to console
+           	log.info(pluginPrefix + "added cake to " + playerName);
+        }
+    }
+	
+	public void onDisable(){
+		//Sort Prefix
+		PluginDescriptionFile pdf = this.getDescription();
+		final String pluginPrefix = "[" + pdf.getName() + "] ";
+		//Send to console
+		log.info(pluginPrefix + "has been disabled.");
+	}
+}
+
